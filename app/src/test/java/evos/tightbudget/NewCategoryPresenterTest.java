f3286b1fd15a95529f67b23b4d1d266effebfb5f
mcdons20 <simon.mcdonough@bbc.co.uk>
2016-03-22 12:47:58 +0000

    Started adding the category mangement stuf

---
+++ app/src/test/java/evos/tightbudget/NewCategoryPresenterTest.java
@@ -0,0 +1,11 @@
+package evos.tightbudget;
+
+/**
+ * Copyright © 2016 Media Applications Technologies. All rights reserved.
+ */
+public class NewCategoryPresenterTest {
+//
+//    @Test
+//    whenACategoryPresenterIsCreated_itSetsTheList
+
+}
